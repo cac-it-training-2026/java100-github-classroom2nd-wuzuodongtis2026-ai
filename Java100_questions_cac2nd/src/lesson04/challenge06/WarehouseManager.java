9c17e331e1b7c142f4a393027f363a5e8d879819
呉作棟（WUZuodong） <WUZuodong@CST0100088>
2026-04-26 22:01:28 +0900

    4月24日100本ノックLesson04のchallenge01から04まで完了。

--- Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
@@ -39,9 +39,11 @@ public class WarehouseManager {
 
 		int[] ABKosanArray = new int[5];
 
-
 		//ここに重複チェックおよび値の代入処理を記述する
+		for (int i = 0; i < 5; i++) {
+			ABKosanArray[i] = (int) (Math.random() * 5) + 1;
 
+		}
 
 		System.out.println("E主任：");
 		System.out.println("AB興産の荷物の入れ替えをお願いします。\n");
@@ -57,9 +59,7 @@ public class WarehouseManager {
 		}
 		System.out.println("\nです。\n");
 
-
 		//ここに値の入れ替え処理を記述する
-
 
 		System.out.println("入れ替え後の状態は、");
 		for (int i = 0; i < ABKosanArray.length; i++) {
